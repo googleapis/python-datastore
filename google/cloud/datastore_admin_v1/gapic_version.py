3dacffdcbed1bb713553da3042e1698bf4a167dc
Anthonios Partheniou <partheniou@google.com>
2022-12-07 16:16:35 +0000

    fix(deps): require google-api-core>=1.34.0,>=2.11.0

--- google/cloud/datastore_admin_v1/gapic_version.py
+++ google/cloud/datastore_admin_v1/gapic_version.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,5 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-__version__ = "0.1.0"  # {x-release-please-version}
+
+__version__ = "2.11.0"  # {x-release-please-version}
