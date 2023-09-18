de79844515cec25859d69236ae88e62d53ed0559
Juan Lara <juanlara@google.com>
2023-09-18 22:53:09 +0000

    Add a test fixture to set up requried indexes.
    remove index.yaml since indexes are created in code

--- samples/snippets/snippets_test.py
+++ samples/snippets/snippets_test.py
@@ -14,7 +14,9 @@
 import os
 
 import backoff
+import google.api_core.exceptions
 from google.cloud import datastore
+from google.cloud import datastore_admin_v1
 import pytest
 
 import snippets
@@ -41,6 +43,37 @@ def client():
     client = CleanupClient(PROJECT)
     yield client
     client.cleanup()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_indexes(request):
+    # Set up required indexes
+    admin_client = datastore_admin_v1.DatastoreAdminClient()
+
+    indexes = []
+    done_property_index = datastore_admin_v1.Index.IndexedProperty(
+        name='done',
+        direction=datastore_admin_v1.Index.Direction.ASCENDING
+    )
+    hour_property_index = datastore_admin_v1.Index.IndexedProperty(
+        name='hours',
+        direction=datastore_admin_v1.Index.Direction.ASCENDING
+    )
+    done_hour_index = datastore_admin_v1.Index(
+        kind='Task',
+        ancestor=datastore_admin_v1.Index.AncestorMode.NONE,
+        properties=[done_property_index, hour_property_index]
+    )
+    indexes.append(done_hour_index)
+
+    for index in indexes:
+        request = datastore_admin_v1.CreateIndexRequest(project_id=PROJECT, index=index)
+        # Create the required index
+        try:
+            admin_client.create_index(request).result()
+        # Pass if the index already exists
+        except (google.api_core.exceptions.AlreadyExists):
+            pass
 
 
 @pytest.mark.flaky
