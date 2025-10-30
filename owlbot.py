3130603db024b7a701ed9605ca821bac54cfc017
Chalmer Lowe <chalmerlowe@google.com>
2025-10-30 10:00:40 -0400

    Apply suggestion from @chalmerlowe

--- owlbot.py
+++ owlbot.py
@@ -104,7 +104,7 @@ templated_files = common.py_library(
     system_test_external_dependencies=["six"],
     cov_level=100,
     unit_test_python_versions=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14"],
-    default_python_version="3.12",
+    default_python_version="3.13",
     system_test_python_versions=["3.12"],
 )
 s.move(
