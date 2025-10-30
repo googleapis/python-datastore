0139e03f56590be7ceb36cc03dbe1424c253de5e
ohmayr <omairn@google.com>
2025-10-30 23:57:07 +0000

    fix replacement

--- owlbot.py
+++ owlbot.py
@@ -217,14 +217,14 @@ s.replace(
 s.replace(
     "noxfile.py",
     """nox.options.sessions = \[
+    "unit",
+    "system",""",
+    """nox.options.sessions = [
     "unit-3.9",
     "unit-3.10",
     "unit-3.11",
     "unit-3.12",
     "unit-3.13",
-    "system",""",
-    """nox.options.sessions = [
-    "unit",
     "system",
     "mypy",""",
 )
